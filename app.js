5cf86f2403b418aa8712400bac7d05e3ce3c387f
Luca De Paul <lucadepaul259@gmail.com>
2024-07-03 21:02:04 -0300

    clases

---
+++ app.js
@@ -0,0 +1,41 @@
+
+// VARIABLES Y TIPOS DE DATOS
+
+let nombre = "pepe"
+let apellido = "perez" //---> string
+
+// alert(nombre)
+
+// alert(apellido)
+
+console.log(nombre)
+// console.log(apellido)
+
+const documento = "123123123" // ---> number
+
+nombre = "juancito"
+
+console.log(nombre)
+
+// let edad_de_la_persona = 20
+let edadDeLaPersona = 20
+console.log(edadDeLaPersona)
+
+// let numero = prompt("ingresa un numero")
+
+// console.log(numero)
+
+let precio = 15.50
+
+// let nombreDeUsuario = prompt("por favor inicia sesion con tu nombre")
+// alert("hola " + nombreDeUsuario + " como estas?")
+
+// let n1 = prompt("ingrese el primer numero")
+// let n2 = prompt("ingrese el segundo numero")
+
+let n1 = Number(prompt("ingrese el primer numero"))
+let n2 = Number(prompt("ingrese el segundo numero"))
+
+let total = n1 + n2
+
+console.log(total)
---
+++ estructurasDeControl.js
@@ -0,0 +1,22 @@
+let nombre = prompt("ingresa tu nombre")
+let edad = Number(prompt("ingresa tu edad"))
+
+alert("gracias por tu compra")
+
+let dia = prompt("ingresa el numero del dia")
+
+switch (dia) {
+    case "1":
+        console.log("lunes")
+        break
+    case "2":
+        console.log("martes")
+        break
+
+}
+
+for (let i = 10 ; i >= 5 ; i--) {
+    console.log(i)
+    
+
+}
